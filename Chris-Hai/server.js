7b6fe3ff3a3f5b0bd5b9ca90abee7f35fe6c39c4
Hai Le <haile@MacBook-Pro-2.local>
2018-08-11 12:11:27 -0700

    created readme file

--- Chris-Hai/server.js
+++ Chris-Hai/server.js
@@ -18,14 +18,15 @@ app.get('/newarticle', (req, res) => {
   res.sendFile('./new.html', {root: "./public"});
 });
 
-app.use((req, res) => {
-  res.status(404).send('404 - File not Found');
-});
 
 app.post('/articles', (req, res) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(req.body);
   res.status(201).json(req.body);
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 - File not Found');
 });
 
 app.listen(PORT, () => {
