9e37ffe5b1305387d8903ef7c434ca36b0583d97
Chris Kozlowski <kozlowskicd@gmail.com>
2018-08-11 11:37:41 -0700

    server.js fleshed out

--- Chris-Hai/server.js
+++ Chris-Hai/server.js
@@ -1,14 +1,32 @@
 'use strict';
 
 const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // REVIEW: POST route needs to parse the body passed in with the request.
 // POST middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('./public'));
+
+app.get('/', (req, res) => {
+  res.sendFile('./public/index.html');
+});
+
+app.get('/newarticle', (req, res) => {
+  res.sendFile("./public/new.html");
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 - File not Found');
+});
 
 app.post('/articles', (request, response) => {
   // REVIEW: This route will receive a new article from the form page, new.html, and log that form data to the console. We will wire this up soon to actually write a record to our persistence layer!
   console.log(request.body);
   response.status(201).json(request.body);
+});
+
+app.listen(PORT, () => {
+  console.log(`server.js listening on port ${PORT}`);
 })
